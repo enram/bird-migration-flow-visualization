abaedacf7cf79ffec28f47d17a303e141b3a585f
Bart Aelterman <bart.aelterman@gmail.com>
2014-05-23 15:30:21 +0200

    Fixes #7 and fixes #13

--- experiments/js/arrow.js
+++ experiments/js/arrow.js
@@ -3,19 +3,14 @@ var w = 400;
 var h = 200;
 var pad = 20;
 
-// test data
-var x1 = 5;
-var y1 = 5;
-var x2 = 6;
-var y2 = 6;
-
 // add svg
 var svg = d3.select("#map").append("svg").attr("width", w + 2*pad).attr("height", h + 2*pad);
 
 // set scales
-var xscale = d3.scale.linear().domain([0,10]).range([0, w]);
-var yscale = d3.scale.linear().domain([0,10]).range([h, 0]);
-
+var xscale = d3.scale.linear().domain([0,10]).range([0, w]); // x scale for the position of the arrow
+var yscale = d3.scale.linear().domain([0,10]).range([h, 0]); // y scale for the position of the arrow
+var zscale = d3.scale.linear().domain([0,100]).range([0, h]); // scale for the length of the arrow
+ 
 // define axes
 var xaxis = d3.svg.axis().scale(xscale).orient("bottom");
 var yaxis = d3.svg.axis().scale(yscale).orient("left");
@@ -42,11 +37,32 @@ var marker = svg.append("marker")
 
 marker.append("path").attr("d", "M 0 0 L 10 5 L 0 10 z");
 
+// define line variable so you can delete it when drawing a new one
+var line = svg.append("line").attr("class", "arrow");
+
 // draw line going from coordinates (5,5) to (6,6)
-svg.append("line")
-    .attr("class", "arrow")
-    .attr("x1", (pad+xscale(x1)))
-    .attr("y1", (pad+yscale(y1)))
-    .attr("x2", (pad+xscale(x2)))
-    .attr("y2", (pad+yscale(y2)))
-    .attr("marker-end", "url(#arr-head)");
+// the arrow is pointing downwards, because the translate will 
+function drawArrow(x, y, len, rotation) {
+    line.attr("x1", pad)
+	.attr("y1", pad)
+	.attr("x2", pad)
+	.attr("y2", (pad+zscale(len)))
+	.attr("marker-end", "url(#arr-head)")
+	.attr("transform", "translate(" + xscale(x) + ", " + yscale(y) + ") rotate(" + (180+rotation) + " " + pad + " " + pad + ")");
+}
+
+
+/* =======================
+ * jQuery stuff
+ * =======================
+ */
+$("#draw-but").on("click", function(event) {
+    console.log("button clicked");
+    var x = parseInt($("#x-in").val());
+    var y = parseInt($("#y-in").val());
+    var len = parseInt($("#len-in").val());
+    var rotation = parseInt($("#rot-in").val());
+    console.log("x: " + x + ", y: " + y + ", length: " + len + ", rotation: " + rotation);
+    drawArrow(x, y, len, rotation);
+});
+
