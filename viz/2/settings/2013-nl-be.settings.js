1a99058cfa6c2d885734e5ad64e8b2758a15c02a
Peter Desmet <peter.desmet.work@gmail.com>
2015-06-11 17:30:15 +0200

    Include settings in html

--- viz/2/settings/2013-nl-be.settings.js
+++
@@ -1,8 +0,0 @@
-var settings = {
-    "datafile": "../../data/2013-nl-be/birds.csv",
-    "radardatafile": "../../data/2013-nl-be/radars.json",
-    "basemapfile": "../../data/2013-nl-be/basemap.topojson",
-    "baselayer_object": "countries",
-    "bbox": [2.5218,49.495223, 7.198506, 53.558092],
-    "date_format": "YY-MM-DD HH:mm"
-};
