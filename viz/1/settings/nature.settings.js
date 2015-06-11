2c90db99ad050eb2a9d1e4f4305f0a1a1c31c94e
Peter Desmet <peter.desmet.work@gmail.com>
2015-06-11 14:01:16 +0200

    Update relative links

---
+++ viz/1/settings/nature.settings.js
@@ -0,0 +1,9 @@
+// Paths are relative to the html file
+var settings = {
+    "datafile": "../data/2013-nl-be/birds.csv",
+    "radardatafile": "../data/2013-nl-be/radars.json",
+    "basemapfile": "1/basemap/basemap.topojson",
+    "baselayer_object": "ne_10m_admin_0_countries",
+    "bbox": [2.5218,49.495223, 7.198506, 53.558092],
+    "date_format": "YYYY-MM-DD HH:mm"
+};
