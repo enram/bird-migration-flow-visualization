e3bb0a16c0ffeb0f025ecf532a2e84e628ad57dc
Peter Desmet <peter.desmet.work@gmail.com>
2015-06-11 16:41:31 +0200

    Update settings links

---
+++ viz/2/settings/2010-new-england.settings.js
@@ -0,0 +1,8 @@
+var settings = {
+    "datafile": "../../data/2010-new-england/birds.csv",
+    "radardatafile": "../../data/2010-new-england/radars.json",
+    "basemapfile": "../../data/2010-new-england/basemap.topojson",
+    "baselayer_object": "countries",
+    "bbox": [-79.74,36.0,-66.76,48.05],
+    "date_format": "YY-MM-DD HH:mm"
+};
