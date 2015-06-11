6f9c68892ff99ebf6f388ffe5639ad5a949bfd29
Peter Desmet <peter.desmet.work@gmail.com>
2015-06-11 17:53:28 +0200

    Set correct default object in New England settings

--- data/2010-new-england/example.settings.js
+++ data/2010-new-england/example.settings.js
@@ -2,7 +2,7 @@ var settings = {
     "datafile": "birds.csv",
     "radardatafile": "radars.json",
     "basemapfile": "basemap.topojson",
-    "baselayer_object": "land",
+    "baselayer_object": "countries",
     "bbox": [-79.74,36.0,-66.76,48.05],
     "date_format": "YY-MM-DD HH:mm"
 };
